6017243698073ddcd9656a5609c97f30d980fc4d
szarasowa <loremipsum@szarasowa.ovh>
2020-11-30 23:19:20 +0100

    Projekt demo - Spring MVC 4 www

---
+++ src/test/java/masterSpringMvc/MasterSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package masterSpringMvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
